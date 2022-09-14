9ff09a30dade80234fa707d0adc60baf457b6025
Mahdi Amereh <mahdieightt@gmail.com>
2022-09-14 10:43:56 +0430

    fix ++

--- app/model/Project.js
+++ app/model/Project.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const projectSchema = new Schema({
-    name: { type: String, default: null },
-    description: { type: String, default: null },
-    is_active: { type: Boolean, default: 1 },
-    is_archive: { type: Boolean, default: null },
-    user_id_created: { type: Number, default: 1 }
-},
-    { timestamps: { createdAt: true } });
+const projectSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        description: { type: String, default: null },
+        is_active: { type: Boolean, default: 1 },
+        deleted_at: { type: Date, default: null },
+        deleted_by: { type: Number, default: null },
+        user_id_created: { type: Number, default: 1 }
+    },
+    { timestamps: { createdAt: true } }
+);
 
 const Project = mongoose.model('projects', projectSchema);
 
