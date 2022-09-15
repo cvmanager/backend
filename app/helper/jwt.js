a7ec29644af4f087725d35bd0a834348042934c7
Mahdi Amereh <mahdieightt@gmail.com>
2022-09-16 01:05:44 +0430

    feat: add verifyToken to jwt helper

--- app/helper/jwt.js
+++ app/helper/jwt.js
@@ -1,5 +1,5 @@
 const JWT = require('jsonwebtoken');
-
+const BadRequestError = require('../middleware/BadRequestError')
 function generateJwtToken(data) {
     return JWT.sign({ sub: data, }, process.env.JWT_SECRET_TOKEN, {
         expiresIn: process.env.JWT_EXPIRATION_TIME_TOKEN
@@ -13,4 +13,19 @@ async function generateJwtRefeshToken(data) {
     });
 }
 
-module.exports = { generateJwtToken, generateJwtRefeshToken }
+async function verifyToken(req, res, next) {
+    try {
+        if (!req.headers.authorization) {
+            throw new BadRequestError('token not sended!');
+        }
+        let token = req.headers.authorization.split(' ')[1];
+        console.log(token);
+        let payload = await JWT.verify(token, process.env.JWT_SECRET_TOKEN);
+        req.user_id = payload.sub;
+        next();
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports = { generateJwtToken, generateJwtRefeshToken, verifyToken }
