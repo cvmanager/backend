d74350cd804a05108b168ecfaa8f0f0253c6dcb9
Mahdi Amereh <mahdieightt@gmail.com>
2022-09-16 00:03:01 +0430

    feat: update routing

--- app/routes/route.js
+++ app/routes/route.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const projectRouting = require('./project-route');
+const authRouting = require('./auth-route');
 
+router.use('/auth', authRouting);
 router.use('/projects', projectRouting);
 
 
