d9de9c5cbbb807e625ae389812e81f4a1ec38dd9
Mahdi Amereh <mahdieightt@gmail.com>
2022-09-16 01:04:28 +0430

    feat: update project model

--- app/model/Project.js
+++ app/model/Project.js
@@ -2,14 +2,32 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const projectSchema = new Schema(
     {
-        name: { type: String, required: true },
-        description: { type: String, default: null },
-        is_active: { type: Boolean, default: 1 },
-        deleted_at: { type: Date, default: null },
-        deleted_by: { type: Number, default: null },
-        user_id_created: { type: Number, default: 1 }
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            default: null
+        },
+        is_active: {
+            type: Boolean,
+            default: 1
+        },
+        deleted_at: {
+            type: Date,
+            default: null
+        },
+        deleted_by: {
+            type: Schema.Types.ObjectId,
+            default: null
+        },
+        created_by: {
+            type: Schema.Types.ObjectId,
+            required: true
+        }
     },
-    { timestamps: { createdAt: true } }
+    { timestamps: { createdAt: 'created_at', updatedAt: "updated_at" } }
 );
 
 const Project = mongoose.model('projects', projectSchema);
