110f52a7645ead7958f72813a6b6c856ca9f5615
Mahdi Amereh <mahdieightt@gmail.com>
2022-09-18 20:41:58 +0430

    fix: fix redis connect

--- app/helper/redis_client.js
+++ app/helper/redis_client.js
@@ -1,21 +1,15 @@
 const { connect } = require('mongoose');
 const redis = require('redis');
 
+const redis_client = redis.createClient(
+    {
+        url: process.env.REDIS_URL,
+        legacyMode: true,
+    }
+);
 
-// async function redis_client() {
-    // connect to redis
-    const redis_client =  redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+redis_client.on("error", (err) => console.log("Error " + err))
+redis_client.connect();
 
-    // await client.on('connect', function () {
-    //     console.log('redis client connected');
-    // });
-    redis_client.on("error", function (err) {
-        console.log("Error " + err);
-    });
-
-    redis_client.connect();
-
-
-// }
 module.exports = redis_client;
 
