fbd61f24180bbb61d20bfcd3fc7957665ebb1d5d
Mahdi Amereh <mahdieightt@gmail.com>
2022-09-20 10:19:38 +0430

    feat: add user controller

---
+++ app/http/controllers/user.controller.js
@@ -0,0 +1,28 @@
+const Controller = require('./controller');
+const User = require('../../models/user.model')
+
+
+class UserController extends Controller {
+
+    async index(req, res, next) {
+
+    }
+
+    async get(req, res, next) {
+
+    }
+
+    async update(req, res, next) {
+
+    }
+
+    async delete(req, res, next) {
+
+    }
+
+    async find(req, res, next) {
+
+    }
+}
+
+module.exports = new UserController;
