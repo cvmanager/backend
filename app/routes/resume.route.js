2b3a642ccb33a05558b7b2732a93483acefd78e1
saeed <saeed.com2000@gmail.com>
2022-09-20 10:38:24 +0430

    get resume saeed

--- app/routes/resume.route.js
+++ app/routes/resume.route.js
@@ -3,5 +3,6 @@ const ResumeController = require('../http/controllers/resume.controller');
 const resumeValidation = require('../validators/resume.validation')
 
 router.post('/', resumeValidation.resume_create, ResumeController.create);
+router.get('/:id', ResumeController.find)
 
 module.exports = router
