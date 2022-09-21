be99b9a5cbdc46a1d31ce54c6490f88b03c7a62a
Mahdi Amereh <mahdieightt@gmail.com>
2022-09-21 10:25:12 +0430

    up ++

--- app/routes/resume.route.js
+++ app/routes/resume.route.js
@@ -2,11 +2,12 @@ const router = require('express').Router()
 const ResumeController = require('../http/controllers/resume.controller');
 const resumeValidation = require('../validators/resume.validation')
 
-router.post('/', resumeValidation.resume_create, ResumeController.create);
-router.patch('/:id', resumeValidation.resume_update, ResumeController.update);
 
-router.get('/', ResumeController.index)
-router.get('/:id', ResumeController.find)
-router.delete('/:id', ResumeController.delete)
+router
+    .get('/', ResumeController.index)
+    .get('/:id', ResumeController.find)
+    .post('/', resumeValidation.resume_create, ResumeController.create)
+    .patch('/:id', resumeValidation.resume_update, ResumeController.update)
+    .delete('/:id', ResumeController.delete)
 
 module.exports = router
