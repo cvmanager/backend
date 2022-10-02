b04b14e29d330401ab9960f12c895f9a1179b7a7
Mahdi Amereh <mahdieightt@gmail.com>
2022-10-02 17:36:00 +0330

    update  user notfound error

--- app/exceptions/UserNotFoundError.js
+++ app/exceptions/UserNotFoundError.js
@@ -1,9 +1,8 @@
 class UserNotFoundError extends Error {
-    message = "User Not Found";
-    // status = 404;
+    message = "user not found";
     constructor(message, validationErr = []) {
         super(message);
-        this.message = message;
+        if(message) this.message = message 
         this.errors = validationErr;
     }
 }
