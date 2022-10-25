6f4059a3dc02cf8ec95c383034e37c3ac39a3c0a
Mahdi Amereh <mahdieightt@gmail.com>
2022-10-25 19:35:45 +0330

    feat: remove soft delet item from count query

--- app/helper/mongoose/base.plugin.js
+++ app/helper/mongoose/base.plugin.js
@@ -1,24 +1,24 @@
 import MongooseDelete from "mongoose-delete"
 import MongoosePaginate from 'mongoose-paginate-v2';
 function basePlugin(schema, options) {
-    
+
     const timestamps = {
         createdAt: true,
         updatedAt: true
     },
-    toJSON = {
-        transform: function (doc, ret, opt) {
-            delete ret['deletedBy']
-            delete ret['deletedAt']
-            delete ret['__v']
-            return ret
+        toJSON = {
+            transform: function (doc, ret, opt) {
+                delete ret['deletedBy']
+                delete ret['deletedAt']
+                delete ret['__v']
+                return ret
+            }
         }
-    }
 
     schema.set('timestamps', timestamps)
     schema.set('toJSON', toJSON)
-    
-    schema.pre(/^find/, function () {
+
+    schema.pre([/^find/, /^count/], function () {
         this.where({ deleted: false });
     });
 
