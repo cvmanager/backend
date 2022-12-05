fa6f02e8a8b22be643bf50a531c5f30995b5fa89
Mehdoub <Mehdoub2020@gmail.com>
2022-12-05 11:51:43 +0330

    feat: company and manager name added to project index response

--- app/models/project.model.js
+++ app/models/project.model.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 import basePlugin from '../helper/mongoose/base.plugin.js';
+import Company from './company.model.js';
 import User from './user.model.js';
 
 const schema = new mongoose.Schema(
@@ -8,12 +9,12 @@ const schema = new mongoose.Schema(
         company_id: {
             type: mongoose.Schema.Types.ObjectId,
             required: true,
-            ref: 'Company'
+            ref: Company
         },
         manager_id: {
             type: mongoose.Schema.Types.ObjectId,
             required: true,
-            ref: 'User'
+            ref: User
         },
         name: {
             type: String,
