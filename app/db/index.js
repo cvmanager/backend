ce2222659d347efe4068875e536e7a7079cbed78
Mahdi Amereh <mahdieightt@gmail.com>
2022-12-08 18:54:49 +0330

    feat: add city model and optimize seed db

--- app/db/index.js
+++ app/db/index.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import Province from '../models/province.model.js'
 import env from '../helper/env.js'
 import provinces from './province.js'
+import City from "../models/city.model.js";
 
 
 
@@ -20,8 +21,30 @@ mongoose.connect(
 
 const seedDB = async () => {
 
+    let provinceArray = [];
+    provinces.map((item) => {
+        provinceArray.push({ name: item.name });
+    })
     await Province.deleteMany({});
-    await Province.insertMany(provinces);
+    await Province.insertMany(provinceArray)
+        .then(async (items) => {
+            let cities = [];
+            items.map((province) => {
+                const provinceInfo = provinces.find(element => element.name = province.name);
+                provinceInfo.cities.map((hi) => {
+                    let object = {
+                        province_id: province._id,
+                        name: hi.name,
+                        latitude: hi.latitude,
+                        longitude: hi.longitude,
+                    }
+                    cities.push(object);
+                })
+
+            })
+            await City.deleteMany({});
+            await City.insertMany(cities);
+        })
 }
 
 seedDB().then(() => {
