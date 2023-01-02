33f598463fa36388c2797901266ee2c2d5bae41d
arashsafari <arash.safari101@gmail.com>
2023-01-02 15:54:03 +0330

    feat:remove deleted from query

--- app/http/controllers/project.controller.js
+++ app/http/controllers/project.controller.js
@@ -170,7 +170,7 @@ class ProjectController extends Controller {
             let user = await User.findById(req.body.manager_id);
             if (!user) throw new NotFoundError("user.errors.user_notfound");
 
-            let manager = await Manager.findOne({ 'entity': "projects", 'entity_id': project.id, 'user_id': user.id, 'deleted': false });
+            let manager = await Manager.findOne({ 'entity': "projects", 'entity_id': project.id, 'user_id': user.id });
             if (manager) throw new BadRequestError("project.errors.the_user_is_currently_an_manager_for_project");
 
             await Manager.create({ user_id: user._id, entity: "projects", entity_id: project._id, created_by: req.user_id });
@@ -202,7 +202,7 @@ class ProjectController extends Controller {
             let user = await User.findById(req.body.manager_id);
             if (!user) throw new NotFoundError("user.errors.user_notfound");
 
-            let manager = await Manager.findOne({ 'entity': "projects", 'entity_id': project.id, 'user_id': user.id, 'deleted': false });
+            let manager = await Manager.findOne({ 'entity': "projects", 'entity_id': project.id, 'user_id': user.id });
             if (!manager) throw new BadRequestError("project.errors.the_user_is_not_an_manager_for_project");
 
             await manager.delete(req.user_id);
