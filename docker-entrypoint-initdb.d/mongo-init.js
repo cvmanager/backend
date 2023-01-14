8cad2beff90193343cb4ca5581844f7ce21471f3
Mahdi Amereh <mahdieightt@gmail.com>
2023-01-15 00:23:31 +0330

    up ++

--- docker-entrypoint-initdb.d/mongo-init.js
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -1,10 +1,10 @@
 import env from '../app/helper/env.js'
 
 print("Started Adding the Users.");
-db = db.getSiblingDB("admin");
+db = db.getSiblingDB(env('DB_NAME'));
 db.createUser({
   user: env('DB_USER_NAME'),
   pwd: env('DB_PASSWORD'),
-  roles: [{ role: "readWrite", db: "admin" }],
+  roles: [{ role: "readWrite", db: env('DB_NAME') }],
 });
 print("End Adding the User Roles.");
