19eb23fc3d2fef5663c417885859a222b6a36c19
Mahdi Amereh <mahdieightt@gmail.com>
2023-01-14 23:16:01 +0330

    up ++

--- app/server.js
+++ app/server.js
@@ -8,7 +8,7 @@ class App {
 
     start() {
         mongoose.set('strictQuery', true);
-        mongoose.connect(`mongodb://${env('DB_USER_NAME')}:${env('DB_PASSWORD')}@${env('DB_HOST')}:${env('DB_PORT')}/${env('DB_PORT')}`, { useNewUrlParser: true, useUnifiedTopology: true })
+        mongoose.connect(`mongodb://${env('DB_USER_NAME')}:${env('DB_PASSWORD')}@${env('DB_HOST')}:${env('DB_PORT')}/${env('DB_NAME')}`, { useNewUrlParser: true, useUnifiedTopology: true })
             .then(data => {
                 console.log('successfully connect to database ...');
                 this.serve();
