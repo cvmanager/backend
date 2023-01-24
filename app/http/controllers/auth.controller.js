fb575826dff577955807f3d9fa25a67cde4eefe9
Mahdi Amereh <mahdieightt@gmail.com>
2023-01-25 00:49:00 +0330

    up ++

--- app/http/controllers/auth.controller.js
+++ app/http/controllers/auth.controller.js
@@ -28,7 +28,7 @@ class AuthController extends Controller {
      */
     async login(req, res, next) {
         try {
-            let user = await User.findOne({ mobile: req.body.mobile, deleted_at: null });
+            let user = await User.findOne({ mobile: req.body.mobile});
             if (!user) throw new NotFoundError('auth.errors.user_not_found');
 
 
