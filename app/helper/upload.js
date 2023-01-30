4024745d24a77215b8aca570699a9b3eed5a5403
1sina1 <sina.bariaji@gmail.com>
2023-01-30 16:53:28 +0330

    crop avatar image

--- app/helper/upload.js
+++ app/helper/upload.js
@@ -1,5 +1,5 @@
+import { extname } from 'path';
 import multer from 'multer';
-import path from 'path';
 import fs from 'fs';
 
 import BadRequestError from '../exceptions/BadRequestError.js';
@@ -7,23 +7,23 @@ import BadRequestError from '../exceptions/BadRequestError.js';
 const config = {
     'image': {
         types: ['image/jpeg', 'image/jpg', 'image/png'],
-        maxSize: 0.6 * 1_000_000, // 1_000_000 Bytes = 1 MB
+        maxSize: 0.6 * 1048576, // 1048576 Bytes = 1 MB
     },
     'file': {
         types: ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'],
-        maxSize: 0.1 * 1_000_000, // 1_000_000 Bytes = 1 MB
+        maxSize: 0.1 * 1048576, // 1048576 Bytes = 1 MB
     }
 }
 
-const createStorage = (entity, fieldName) => {
+const createStorage = (path, fieldName) => {
     let basePath = './public';
-    let realPath = `/uploads/${entity}/`;
+    let realPath = '/uploads' + path;
     let fullPath = basePath + realPath;
 
     return multer.diskStorage({
         destination: fullPath,
         filename: (req, file, cb) => {
-            let extension = path.extname(file.originalname);
+            let extension = extname(file.originalname);
             let name = req.user_id._id;
 
             if (fs.existsSync(fullPath + name + extension)) {
@@ -37,19 +37,26 @@ const createStorage = (entity, fieldName) => {
         }
     })
 }
-function Upload(entity, field, type) {
-    let fileConfig = config[type];
-    return multer({
-        storage: createStorage(entity, field),
-        fileFilter: (req, file, cb) => {
-            if (!fileConfig.types.includes(file.mimetype)) {
-                return cb(new BadRequestError(`exceptions.valid_${type}_format`));
-            }
-            cb(null, true);
-        },
-        limits: { fileSize: fileConfig.maxSize }
-    }).single(field)
-}
 
+function Upload(entity, field, type) {
+    return (req, res, next) => {
+        let fileConfig = config[type];
+        const upload = multer({
+            storage: createStorage(entity, field),
+            fileFilter: (req, file, cb) => {
+                if (!fileConfig.types.includes(file.mimetype)) {
+                    return cb(new BadRequestError(`exceptions.valid_${type}_format`));
+                }
+                cb(null, true);
+            },
+            limits: { fileSize: fileConfig.maxSize }
+        }).single(field)
+    
+        upload(req, res, (error) => {
+            if (error) next(new BadRequestError('system.errors.' + error.code || "upload_file"), [error])
+            return next()
+        })
+    }
+}
 
 export { Upload };
