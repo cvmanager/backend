115938a579028f9cba36550ed7f94e53f0714f44
manimohamadi <mohamadimahdi@yahoo.com>
2023-02-19 18:14:11 +0330

    changed condition

--- app/helper/service/resume.service.js
+++ app/helper/service/resume.service.js
@@ -1,11 +1,12 @@
+const endOfResumeStatus = ['rejected', 'hired'];
 
-const setProccessDuration = async (resume) => {
-    if (resume.status == 'rejected' || resume.status == 'hired') {
-        let createdAtTimeStamp = Math.floor((new Date(resume.createdAt).getTime()) / 1000)
-        let rejectedOrHiredTimeStamp = Math.floor((new Date().getTime()) / 1000)
-        let processDuration = rejectedOrHiredTimeStamp - createdAtTimeStamp;
-        resume.process_duration = processDuration;
-        await resume.save();
+const setProccessDuration = async(resume) => {
+    if (endOfResumeStatus.includes(resume.status)) {
+        let createdAtTimeStamp = Math.floor(new Date(resume.createdAt).getTime() / 1000, )
+        let rejectedOrHiredTimeStamp = Math.floor(new Date().getTime() / 1000)
+        let processDuration = rejectedOrHiredTimeStamp - createdAtTimeStamp
+        resume.process_duration = processDuration
+        await resume.save()
     }
 }
 
