78ef59c8a7bcfb990fb4ddfd13f5d82a4e45e93a
arashsafari <arash.safari101@gmail.com>
2023-04-25 09:56:28 +0330

    feat:convert name to lower case

--- app/middlewares/lowerCase.middleware.js
+++ app/middlewares/lowerCase.middleware.js
@@ -1,13 +1,32 @@
 
 async function fillFullName(req, res, next) {
     try {
-        if(req.body.firstname != undefined) req.body.firstname  = req.body.firstname.toLowerCase();
-        if(req.body.lastname != undefined) req.body.lastname  = req.body.lastname.toLowerCase();
-        if(req.body.firstname != undefined && req.body.lastname != undefined) req.body.fullname = req.body.firstname + ' ' + req.body.lastname;
+        if (req.body.firstname != undefined) req.body.firstname = req.body.firstname.toLowerCase();
+        if (req.body.lastname != undefined) req.body.lastname = req.body.lastname.toLowerCase();
+        if (req.body.firstname != undefined && req.body.lastname != undefined) req.body.fullname = req.body.firstname + ' ' + req.body.lastname;
         next();
     } catch (err) {
         next(err);
     }
 }
 
-export { fillFullName }
+async function convertCompanyFieldToLowerCase(req, res, next) {
+    try {
+        if (req.body.name != undefined) req.body.name = req.body.name.toLowerCase();
+        next();
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function convertProjectFieldToLowerCase(req, res, next) {
+    try {
+        if (req.body.name != undefined) req.body.name = req.body.name.toLowerCase();
+        next();
+    } catch (err) {
+        next(err);
+    }
+}
+
+
+export { fillFullName, convertCompanyFieldToLowerCase, convertProjectFieldToLowerCase }
