101f2ef1c9d000a381c4dd5d551f039153d92087
arashsafari <arash.safari101@gmail.com>
2023-05-17 15:50:02 +0330

    feat:sen notify from notification table

--- app/helper/service/notification.service.js
+++ app/helper/service/notification.service.js
@@ -1,6 +1,8 @@
 import autoBind from "auto-bind";
 import Notification from "../../models/notification.model.js";
 import ServiceBase from "./base.service.js";
+import { sendNotificationToClient } from '../notify.js'
+import userService from "./user.service.js";
 
 class NotificationService extends ServiceBase {
     constructor(model) {
@@ -20,6 +22,29 @@ class NotificationService extends ServiceBase {
             created_by: req.user._id,
         };
         await this.create(params);
+    }
+
+    async sendNotification(limit) {
+        let notifications = await Notification
+            .find({ "send_state": false })
+            .limit(limit)
+
+        for (let notification of notifications) {
+
+            let fcmTokens = await userService.getFCMTokensOfUser(notification.user_id);
+            if (fcmTokens.length > 0) {
+                const notificationData = {
+                    title: notification.title,
+                    body: notification.body,
+                };
+                sendNotificationToClient(fcmTokens, notificationData);
+            }
+
+            notification.send_state = true
+            notification.send_time = new Date()
+            notification.save()
+
+        }
     }
 
 }
