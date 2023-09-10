a6c8a3d5a432fa956569cf4f61e591cd833cdeeb
Ali ML <gameali774@gmail.com>
2023-09-10 08:40:07 +0000

    Add new file

---
+++ check2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Run the command and capture the output
+output=$(docker exec cv_app npm run test 2>&1)
+# Get the exit code
+exit_code=$?
+# Get the execution time in seconds
+start_time=$(date +%s)
+end_time=$(date +%s)
+duration=$((end_time - start_time))
+
+# Format the output as JUnit XML
+cat <<EOF > report.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<testsuite name="images" tests="1" failures="$((exit_code != 0 ? 1 : 0))" errors="0" skipped="0" time="$duration">
+  <testcase name="images" classname="images" time="$duration">
+    $(if [ $exit_code != 0 ]; then echo "<failure message=\"Command failed with exit code $exit_code\">$output</failure>"; fi)
+    <system-out>$output</system-out>
+  </testcase>
+</testsuite>
+EOF
